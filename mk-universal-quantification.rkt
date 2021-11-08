53d5c69d8c25a5eec2d82ff1ddda295a4dab0925
Jonathan Martin <mac.martin@mail.utoronto.ca>
2021-11-07 21:42:49 -0500

    first draft of domain negation

---
+++ mk-universal-quantification.rkt
@@ -0,0 +1,25 @@
+#lang racket
+(provide domain-satisfiable)
+(require "microk-fo.rkt")
+
+;; universal quantification
+(define (domain-candidates domain state)
+  (pause state domain))
+
+(define (negate-domain g v)
+  (match (g)
+    ((conj g1 g2) (let ((g1 (negate-domain g1 v)) (g2 (negate-domain g2 v))) (if g1 (disj g1 g2) g2)))
+    ((disj g1 g2) (let ((g1 (negate-domain g1 v)) (g2 (negate-domain g2 v))) (if g2 (conj g1 g2) g2)))
+    ((== t1 t2) (if (or (eqv? t1 v) (eqv? t2 v)) #f (=/= t1 t2)))
+    ((=/= t1 t2) (if (or (eqv? t1 v) (eqv? t2 v)) #f (== t1 t2)))
+    (_ #f)
+    ; TODO : Add type negations 
+    ))
+
+#|
+(forall (v) domain body)
+
+(mplus (pause st (negate-domain domain v))
+       other-stuff)
+
+|#
