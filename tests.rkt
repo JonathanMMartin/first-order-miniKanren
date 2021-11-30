1616675fd6c134fa1a976c0e2d76bcfd50d8b5b0
Maryam Gohargani <60069680+magnesium4@users.noreply.github.com>
2021-11-29 20:40:04 -0500

    failing test for forall

--- tests.rkt
+++ tests.rkt
@@ -234,13 +234,13 @@
   (run* (x) (forall (v) (== x 1)))
   '((1)))
 
-; (test 'forall-test-1
-;   (run* (x) (forall (v) (== 0 1)))
-;   '())
+(test 'forall-test-1
+  (run* (x) (forall (v) (== 0 1)))
+  '())
 
-; (test 'forall-test-2
-;   (run 1 (x) (forall (v) (== v 1)))
-;   '())
+(test 'forall-test-2
+  (run 1 (x) (forall (v) (== v 1)))
+  '())
 
 ; (test 'forall-test-3
 ;   (run 1 (x) (forall (v) (== x v)))
