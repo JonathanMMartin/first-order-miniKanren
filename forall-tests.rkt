a15cec414b8645b0c044d9c739d3c0425c68ba9a
Jonathan Martin <mac.martin@mail.utoronto.ca>
2021-12-17 18:11:35 -0500

    add more forall tests based on dec 17 meeting

--- forall-tests.rkt
+++ forall-tests.rkt
@@ -2,21 +2,53 @@
 (newline)
 
 (test 'forall-test-0
-  (run* (x) (forall (v) (== x 1)))
-  '((1)))
-
-(test 'forall-test-1
   (run* (x) (forall (v) (== 0 1)))
   '())
 
-(test 'forall-test-2
+(test 'forall-test-1
+  (run* (x) (forall (v) (== x 1)))
+  '((1)))
+
+(test 'forall-test-3
+  (run 1 (x) (forall (v) (== x x)))
+  '((_.0)))
+
+(test 'forall-test-3 
+  (run 1 (x) (forall (v) (conj (== x 0) (== x 1))))
+  '())
+
+(test 'forall-test-4
+  (run 2 (x) (forall (v) (disj (== x 0) (== x 1))))
+  '((0) (1)))
+
+(test 'forall-test-5
   (run 1 (x) (forall (v) (== v 1)))
   '())
 
-(test 'forall-test-3
+(test 'forall-test-6
+  (run 1 (x) (forall (v) (=/= v 1)))
+  '())
+
+;(test 'forall-test-7
+;  (run 1 (x) (forall (v) (disj (== v 1) (=/= v 1))))
+;  '((_.0)))
+
+;(test 'forall-test-8
+;  (run 1 (x) (forall (v) (disj (== v 1) (imply (== v 1) (== 2 3)))))
+;  '((_.0)))
+
+;(test 'forall-test-9
+;  (run 1 (x) (forall (v) (disj (== v 1) (== x 1))))
+;  '((1)))
+
+(test 'forall-test-10
+  (run 1 (x) (forall (v) (conj (=/= x x) (== v 1))))
+  '(()))
+
+(test 'forall-test-11
   (run 1 (x) (forall (v) (== x v)))
   '())
 
-;(test 'forall-test-4
+;(test 'forall-test-12
 ;  (run 1 (x) (forall (v) (imply (=/= x v) (=/= 1 v))))
 ;  '(1))
