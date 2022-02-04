03f8e7a014c756feed5735f172ca5153c4218be2
Jonathan Martin <mac.martin@mail.utoronto.ca>
2022-02-04 12:34:35 -0500

    fix test description in tests-pairo

--- tests-pairo.rkt
+++ tests-pairo.rkt
@@ -5,14 +5,14 @@
   (run* (x) (pairo x))
   '(#s(Ans (_.0) ((pair _.0)))))
 
-(test 'numbero-1
+(test 'pairo-1
   (run* (x) (== (cons 1 2) x) (pairo x))
   '(((1 . 2))))
 
-(test 'numbero-2
+(test 'pairo-2
   (run* (x) (pairo x) (== (cons 1 2) x))
   '(((1 . 2))))
 
-(test 'numbero-3
+(test 'pairo-3
   (run* (x) (=/= x (cons 5 6)) (pairo x)) 
   '(#s(Ans (_.0) ((=/= ((_.0 (5 . 6)))) (pair _.0)))))
